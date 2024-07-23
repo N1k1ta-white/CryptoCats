ce3e353ddbed97f4c025c91b5296eea73542222a
Nikita <65460812+N1k1ta-white@users.noreply.github.com>
2024-07-23 22:45:09 +0000

    fully works TWA API

--- frontend/CryptoCats/src/app/main/main.component.ts
+++ frontend/CryptoCats/src/app/main/main.component.ts
@@ -1,39 +1,31 @@
 import { Component, Inject, inject, OnInit } from '@angular/core';
 import { Telegram } from '@twa-dev/types';
 
-declare global {
-  interface Window {
-    Telegram: any;
-  }
-}
-
-
 @Component({
   selector: 'app-main',
   standalone: true,
   imports: [],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.css'
+  styleUrl: './main.component.css',
 })
 export class MainComponent implements OnInit {
-
   // constructor(private telegramService : TelegramService) {
   // }
   // private window;
-  // public info : any;
+  public info: any;
 
   // constructor(@Inject(DOCUMENT) private win : any) {
   //   this.window = win;
   // }
 
   ngOnInit(): void {
-    console.log(window?.Telegram?.WebApp?.initDataUnsafe);
-
+    this.info = JSON.stringify(window?.Telegram?.WebApp?.initDataUnsafe);
+    console.log(this.info);
 
     // console.log("HI");
     // let info = this.window.Telegram;
     // console.log(info);
-      // let info = window.Telegram.init()q
-      // console.log(info);
+    // let info = window.Telegram.init()q
+    // console.log(info);
   }
 }
